369a09f30197acafbca75cca20c3e8c297bf2753
bharathjinka09 <bharathjinka09@gmail.com>
2020-09-13 18:09:27 +0530

    Autocomplete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'material-angular';
   notifications = 2;
   showSpinner = false;
+  opened = false;
+
+  selectedValue: string;
+  multiSelectedValue: string;
+
+  options: string[] = ['Angular', 'React', 'Vue'];
+
+  objectOptions = [
+    { name: 'Angular' },
+    { name: 'Angular Material' },
+    { name: 'React' },
+    { name: 'Vue' },
+  ];
+
+  myControl = new FormControl();
+  filteredOptions: Observable<string[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter((option) =>
+      option.toLowerCase().includes(filterValue)
+    );
+  }
+
+  logChange(index) {
+    console.log(index);
+  }
 
   loadData() {
     this.showSpinner = true;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule,FormsModule,ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
