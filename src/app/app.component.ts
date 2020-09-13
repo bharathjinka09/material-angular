d1517deb1ecc529ef1e89f5c2fe518a4a3901959
bharathjinka09 <bharathjinka09@gmail.com>
2020-09-13 13:50:11 +0530

    Added spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'material-angular';
+  notifications = 2;
+  showSpinner = false;
+
+  loadData() {
+    this.showSpinner = true;
+    setTimeout(() => {
+      this.showSpinner = false;
+    }, 3000);
+  }
 }
